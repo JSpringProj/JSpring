7f68d7007dcaf9decff206d15534984d7c1a1349
Ganesh Sekaran <gsekaran@cisco.com>
2015-08-21 10:53:35 +0530

    added

--- JSpring/src/com/jspring/annotations/Cacheable.java
+++ JSpring/src/com/jspring/annotations/Cacheable.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Cachable {
+public @interface Cacheable {
 
-	
+	String cacheRegion() default "default";
 }
