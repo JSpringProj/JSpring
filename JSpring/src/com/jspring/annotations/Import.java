d867aade626fa21e407bd383e00715e7985d849c
Ganesh Sekaran <gsekaran@cisco.com>
2015-08-21 16:23:09 +0530

    added

--- JSpring/src/com/jspring/annotations/Import.java
+++ JSpring/src/com/jspring/annotations/Import.java
@@ -5,9 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Component
-public @interface Configuration {
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Import {
 
+	Class<?>[] imports();
 }
